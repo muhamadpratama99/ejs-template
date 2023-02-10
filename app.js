f84113af6323a8ecd967a5c02437099ba20f7f7a
Muhamad Ramdhanny Pratama <pratamaramdhanny@gmail.com>
2023-02-11 03:36:23 +0700

    Mengintegrasikan app.js ke database mongodb menggunakan odm mongoose

--- app.js
+++ app.js
@@ -1,38 +1,81 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = 3000;
+const mongoose = require('mongoose');
+
 const app = express();
-
-
-let items = ["Welcome to my todo-list app!", "Type here to add list", "<= Click here to delete your list"];
-let workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://127.0.0.1/todoListDB", {useNewUrlParser: true});
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to todolist app!"
+});
+
+const item2 = new Item({
+    name: "Hit + button to add list"
+});
+
+const item3 = new Item({
+    name: "<= Click here to delete list"
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+
 app.get('/', function(req, res) {
-    let today = new Date();
-    let options = {weekday: "long", day: "numeric", month:"long"};
-    let day = today.toLocaleDateString("en-us", options);
-    res.render("list", {listTitle : day, newListItem : items});
+
+    Item.find({}, function(err, foundItems) {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, function(err){
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully saved default items to database");
+                }
+            });
+            res.redirect('/');
+        } else {
+            res.render("list", {listTitle : "Today", newListItems: foundItems});
+        }
+        
+    });
+   
 });
 
 app.post('/', function(req, res) {
-    let item = req.body.newItem;
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect('/work');
-    } else {
-        items.push(item);
-        res.redirect('/');
-    }
+    const itemName = req.body.newItem;
+    const item = new Item({
+        name: itemName
+    });
+    item.save();
+    console.log("Successfully added new item to dbs");
+    res.redirect('/')
 });
 
-app.get('/work', function(req, res) {
-    res.render("list", {listTitle : "Work List", newListItem: workItems});
+app.post('/delete', function(req,res) {
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Successfully deleted one list by id");
+            res.redirect('/');
+        }
+    });
+    console.log(req.body);
 });
 
-app.listen(PORT, function(){
+
+app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
